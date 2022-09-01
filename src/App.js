be8e94c01b6e24798bc0651970c4cf9c3fb2dce4
Zane Fink <zanefink@Zanes-MacBook-Pro.local>
2022-09-01 17:28:33 -0400

    fixed image thing

--- src/App.js
+++ src/App.js
@@ -281,7 +281,7 @@ const handleUpdateHike = (hikes)=>{
           setHike(response.data);
         })
   })
-  setDisplayEditHike(!displayEditHike)
+  showEditImages()
 
 }
 
@@ -372,7 +372,7 @@ Images: <input type='text' onChange={handleNewHikeImage}/><br/>
         length: <input type='text' placeholder={hikes.length} onChange={handleUpdateHikeLength}/><br/>
         elevationGain: <input type='number' placeholder={hikes.elevationGain} onChange={handleUpdateHikeElevation}/><br/>
         difficulty: <input type='text' placeholder={hikes.difficulty} onChange={handleUpdateHikeDifficulty}/><br/>
-        image: <input type='text' placeholder='image url' onChange={handleUpdateHikeImage}/><br/>
+        image: <input type='text' defaultValue={hikes.imageArray} onChange={handleUpdateHikeImage}/><br/>
 
 
         <button onClick={(e) => {handleUpdateHike(hikes)} } type='submit' value='Update Hike'>
